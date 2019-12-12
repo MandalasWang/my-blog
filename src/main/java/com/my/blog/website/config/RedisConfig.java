449cbf964c9edfc37707218d4c6dfdaf6ba4623e
有缘 <single_violet@163.com>
2019-12-12 09:42:41 +0800

    add: 2019/12/12 王博渊 add Redis

---
+++ src/main/java/com/my/blog/website/config/RedisConfig.java
@@ -0,0 +1,113 @@
+package com.my.blog.website.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+
+/**
+ * @author wyy
+ * @version 1.0
+ * @date 2019/9/30 10:23
+ * @description
+ **/
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisConfig {
+    /**
+     * Logger
+     */
+    private static final Logger lg = LoggerFactory.getLogger(RedisConfig.class);
+
+    /**
+     * 注入bean
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    /**
+     * 自动生成key
+     * @return
+     */
+    @Bean
+    public KeyGenerator simpleKeyGenerator() {
+        return (o, method, objects) -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(o.getClass().getSimpleName());
+            stringBuilder.append(".");
+            stringBuilder.append(method.getName());
+            stringBuilder.append("[");
+            for (Object obj : objects) {
+                stringBuilder.append(obj.toString());
+            }
+            stringBuilder.append("]");
+
+            return stringBuilder.toString();
+        };
+    }
+
+    /**
+     * 自定义缓存管理
+     * @param redisConnectionFactory
+     * @return
+     */
+//    @Bean
+//    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+//        return new RedisCacheManager(
+//                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+//                // 默认策略，未配置的 key 会使用这个
+//                this.getRedisCacheConfigurationWithTtl(600),
+//                // 指定 key 策略
+//                this.getRedisCacheConfigurationMap()
+//        );
+//    }
+
+//    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+//        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>(16);
+//        redisCacheConfigurationMap.put("UserInfoList", this.getRedisCacheConfigurationWithTtl(100));
+//        redisCacheConfigurationMap.put("UserInfoListAnother", this.getRedisCacheConfigurationWithTtl(18000));
+//
+//        return redisCacheConfigurationMap;
+//    }
+
+    /**
+     * 自定设置过期时间
+     * @param seconds
+     * @return
+     */
+//    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(om);
+//
+//        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+//        redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(
+//                RedisSerializationContext
+//                        .SerializationPair
+//                        .fromSerializer(jackson2JsonRedisSerializer)
+//        ).entryTtl(Duration.ofSeconds(seconds));
+//
+//        return redisCacheConfiguration;
+//    }
+
+}
